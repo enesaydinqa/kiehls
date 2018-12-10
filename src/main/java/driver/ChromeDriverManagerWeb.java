515bcd341df5bd5cac5bfee1b4b1facfedd86b06
enes.aydin <Deadlydream61>
2018-12-10 23:14:44 +0300

    update 2

--- src/main/java/driver/ChromeDriverManagerWeb.java
+++ src/main/java/driver/ChromeDriverManagerWeb.java
@@ -3,8 +3,10 @@ package driver;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,71 +22,64 @@ import properties.LoadProperties;
 
 public class ChromeDriverManagerWeb extends DriverManager
 {
-    private Logger LOGGER = Logger.getLogger(ChromeDriverManagerWeb.class.getName());
+    private Logger LOGGER = Logger.getLogger(ChromeDriverManagerResponsive.class.getName());
 
-    protected void createDriver() throws Exception
+    @Override
+    public void createDriver() throws Exception
     {
-        try{
-            Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
 
-            Local browserStackLocal = new Local();
-            HashMap<String, String> browserStackLocalArgs = new HashMap<>();
-            browserStackLocalArgs.put("key", AUTOMATE_KEY);
-            browserStackLocalArgs.put("forcelocal", "true");
-            browserStackLocalArgs.put("forceproxy", "true");
-            browserStackLocalArgs.put("force", "true");
-            browserStackLocalArgs.put("v", "true");
-            String host = seleniumProxy.getHttpProxy().substring(0, seleniumProxy.getHttpProxy().indexOf(":"));
-            String port = seleniumProxy.getHttpProxy().substring(seleniumProxy.getHttpProxy().indexOf(":") + 1);
-            browserStackLocalArgs.put("-local-proxy-host", host);
-            browserStackLocalArgs.put("-local-proxy-port", port);
-            browserStackLocal.start(browserStackLocalArgs);
+        Local browserStackLocal = new Local();
+        HashMap<String, String> browserStackLocalArgs = new HashMap<>();
+        browserStackLocalArgs.put("key", AUTOMATE_KEY);
+        browserStackLocalArgs.put("forcelocal", "true");
+        browserStackLocalArgs.put("forceproxy", "true");
+        browserStackLocalArgs.put("force", "true");
+        browserStackLocalArgs.put("v", "true");
+        String host = seleniumProxy.getHttpProxy().substring(0, seleniumProxy.getHttpProxy().indexOf(":"));
+        String port = seleniumProxy.getHttpProxy().substring(seleniumProxy.getHttpProxy().indexOf(":") + 1);
+        browserStackLocalArgs.put("-local-proxy-host", host);
+        browserStackLocalArgs.put("-local-proxy-port", port);
+        browserStackLocal.start(browserStackLocalArgs);
 
-            LOGGER.info("This Execute Browser Host --> " + host);
-            LOGGER.info("This Execute Browser Port --> " + port);
+        LOGGER.info("This Execute Browser Host --> " + host);
+        LOGGER.info("This Execute Browser Port --> " + port);
 
-            String[] switches = {"--ignore-certificate-errors"};
+        String[] switches = {"--ignore-certificate-errors"};
 
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments("disable-infobars");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("disable-infobars");
 
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            capabilities.setCapability("chrome.switches", Arrays.asList(switches));
-            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-            capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-            capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
-            capabilities.setCapability("browser", "Chrome");
-            capabilities.setCapability("browser_version", "70.0");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("chrome.switches", Arrays.asList(switches));
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
 
-            if (REMOTE_TEST.equals("true"))
-            {
-                capabilities.setCapability("browserstack.local", "true");
-                capabilities.setBrowserName("chrome");
-
-                driver = new RemoteWebDriver(new URL(BROWSER_STACK_URL), capabilities);
-
-            }
-            else
-            {
-                if (Platform.getCurrent().is(Platform.MAC))
-                {
-                    System.setProperty("webdriver.chrome.driver", LoadProperties.config.getProperty("forMacChromeDriver"));
-                }
-                else if (Platform.getCurrent().is(Platform.WINDOWS))
-                {
-                    System.setProperty("webdriver.chrome.driver", LoadProperties.config.getProperty("forWinChromeDriver"));
-                }
-
-                driver = new ChromeDriver(capabilities);
-            }
-        }finally
+        if (REMOTE_TEST.equals("true"))
         {
-            /**
-             * There is no action
-             */
-        }
+            capabilities.setCapability("browserstack.local", "true");
+            capabilities.setBrowserName("chrome");
 
+            driver = new RemoteWebDriver(new URL(BROWSER_STACK_URL), capabilities);
+
+        }
+        else
+        {
+
+            if (Platform.getCurrent().is(Platform.MAC))
+            {
+                System.setProperty("webdriver.chrome.driver", LoadProperties.config.getProperty("forMacChromeDriver"));
+            }
+            else if (Platform.getCurrent().is(Platform.WINDOWS))
+            {
+                System.setProperty("webdriver.chrome.driver", LoadProperties.config.getProperty("forWinChromeDriver"));
+            }
+
+            driver = new ChromeDriver(capabilities);
+
+        }
 
     }
 
