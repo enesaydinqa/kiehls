d814f8e65817edefde894cee6f09caee33cb52c9
enes.aydin <enes.aydin@sahibinden.com>
2018-12-22 13:28:11 +0300

    -

--- src/test/java/selenium/AllTestRunner.java
+++ src/test/java/selenium/AllTestRunner.java
@@ -4,9 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({selenium.galen.NyxCostemicMainPageLayout.class, selenium.responsive.NYXCostemicMainPage.class,
-        selenium.responsive.NYXCostemicProductPage.class, selenium.web.NYXCostemicMainPage.class,
-        selenium.web.NYXCostemicProductPage.class})
+@Suite.SuiteClasses({selenium.galen.NyxCostemicMainPageLayout.class})
 public class AllTestRunner
 {
+
+    /*
+    @Suite.SuiteClasses({selenium.galen.NyxCostemicMainPageLayout.class, selenium.responsive.NYXCostemicMainPage.class,
+        selenium.responsive.NYXCostemicProductPage.class, selenium.web.NYXCostemicMainPage.class,
+        selenium.web.NYXCostemicProductPage.class})
+     */
 }
