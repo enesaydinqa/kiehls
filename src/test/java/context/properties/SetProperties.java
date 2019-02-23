7f6d7e232d9a6d27ca13ceb0b19030b6c215b144
enes.aydin <enes.aydin@sahibinden.com>
2019-02-23 18:47:43 +0300

    allure reports

--- src/test/java/context/properties/SetProperties.java
+++ src/test/java/context/properties/SetProperties.java
@@ -48,8 +48,5 @@ public class SetProperties {
         prop.setProperty("browserstack.url", "https://" + System.getProperties().getProperty("username") + ":" + System.getProperties().getProperty("automate.key") + "@hub-cloud.browserstack.com/wd/hub");
 
         prop.store(output, "NYX Costemics Regression Test Configuration");
-
-
-        System.setProperty("allure.results.directory", System.getProperty("user.dir") + "/target");
     }
 }
