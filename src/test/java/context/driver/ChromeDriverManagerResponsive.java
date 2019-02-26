772e4c81b2d8ee82ecb161dfdfde7482974bd92e
enes.aydin <enes.aydin@sahibinden.com>
2019-02-26 22:39:05 +0300

    Capability

--- src/test/java/context/driver/ChromeDriverManagerResponsive.java
+++ src/test/java/context/driver/ChromeDriverManagerResponsive.java
@@ -73,8 +73,11 @@ public class ChromeDriverManagerResponsive extends DriverManager
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-        //capabilities.setCapability("browser", "Chrome");
         capabilities.setCapability("platform", "MAC");
+        capabilities.setCapability("realMobile", "true");
+        capabilities.setCapability("device", "Galaxy A8");
+        capabilities.setCapability("os_version", "8.1");
+        capabilities.setCapability("browserstack.appium_version", "1.8.0");
 
         if (withProxy)
         {
