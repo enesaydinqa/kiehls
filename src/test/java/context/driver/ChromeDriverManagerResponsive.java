9d95d80cb95c3c400c37e69cf34fcf5e42d78bf5
enes.aydin <enes.aydin@sahibinden.com>
2019-03-02 12:31:56 +0300

    network profile removed

--- src/test/java/context/driver/ChromeDriverManagerResponsive.java
+++ src/test/java/context/driver/ChromeDriverManagerResponsive.java
@@ -98,7 +98,6 @@ public class ChromeDriverManagerResponsive extends DriverManager
             capabilities.setCapability("realMobile", "true");
             capabilities.setCapability("device", "iPhone 6S Plus");
             capabilities.setCapability("os_version", "11");
-            capabilities.setCapability("browserstack.networkProfile", "4g-lte-high-latency");
             capabilities.setCapability("acceptSslCerts", "true");
             //capabilities.setCapability("browserstack.debug", "true");
             //capabilities.setCapability("browserstack.console", "warnings");
