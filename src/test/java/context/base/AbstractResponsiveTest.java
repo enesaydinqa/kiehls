9007e4ef7174d2ee3ac5f35266eea9e3b143e1e2
enes.aydin <enes.aydin@sahibinden.com>
2019-03-02 11:55:53 +0300

    -

--- src/test/java/context/base/AbstractResponsiveTest.java
+++ src/test/java/context/base/AbstractResponsiveTest.java
@@ -101,7 +101,7 @@ public abstract class AbstractResponsiveTest extends AbstractLayoutDesignTest
             Runtime.getRuntime().exec("browserstacklocal/BrowserStackLocal --key " + System.getProperty("access.key"));
 
             proxy = new BrowserMobProxyServer();
-            proxy.start(3030);
+            proxy.start(9050);
             proxy.enableHarCaptureTypes(CaptureType.REQUEST_BINARY_CONTENT);
             proxy.newHar();
         }
