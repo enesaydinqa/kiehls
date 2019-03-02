91b3e6588fc0b34bdf50debcba1307450e6ebf8e
enes.aydin <enes.aydin@sahibinden.com>
2019-03-02 13:15:49 +0300

    -

--- src/test/java/selenium/tests/adaptive/NYXCosmeticsProductTest.java
+++ src/test/java/selenium/tests/adaptive/NYXCosmeticsProductTest.java
@@ -3,15 +3,19 @@ package selenium.tests.adaptive;
 import context.base.AbstractNYXCostemicResponsiveTest;
 import context.base.Description;
 import context.helper.JSHelper;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import selenium.pages.UrlFactory;
 import selenium.pages.mobile.MainPageResponsivePage;
 
+import java.time.Duration;
 import java.util.stream.IntStream;
 
 public class NYXCosmeticsProductTest extends AbstractNYXCostemicResponsiveTest
@@ -53,7 +57,8 @@ public class NYXCosmeticsProductTest extends AbstractNYXCostemicResponsiveTest
                     {
                         WebElement theNewestFrom = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[3]/div[" + i + "]"));
                         WebElement theNewestTo = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[3]/div[" + (i + 1) + "]"));
-                        dragAndDrop(theNewestFrom, theNewestTo);
+
+                        if(!Boolean.parseBoolean(System.getProperty("remote.test"))) {dragAndDrop(theNewestFrom, theNewestTo);}
 
                         WebElement dynamicGetProductPrice = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[3]/div[" + i + "]//div[contains(text(), ' TL')]"));
 
@@ -72,7 +77,8 @@ public class NYXCosmeticsProductTest extends AbstractNYXCostemicResponsiveTest
                     {
                         WebElement bestSellersFrom = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[4]/div[" + i + "]"));
                         WebElement bestSellersTo = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[4]/div[" + (i + 1) + "]"));
-                        dragAndDrop(bestSellersFrom, bestSellersTo);
+
+                        if(!Boolean.parseBoolean(System.getProperty("remote.test"))) {dragAndDrop(bestSellersFrom, bestSellersTo);}
 
                         WebElement dynamicGetProductPrice = driver.findElement(By.xpath("(//div[@class='swiper-wrapper'])[3]/div[" + i + "]//div[contains(text(), ' TL')]"));
 
