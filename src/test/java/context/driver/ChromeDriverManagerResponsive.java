b36aa7284f966ffdb30901cd35de340f50ae0361
enes.aydin <enes.aydin@sahibinden.com>
2019-03-02 10:44:24 +0300

    update

--- src/test/java/context/driver/ChromeDriverManagerResponsive.java
+++ src/test/java/context/driver/ChromeDriverManagerResponsive.java
@@ -99,7 +99,7 @@ public class ChromeDriverManagerResponsive extends DriverManager
             capabilities.setCapability("browserstack.debug", "true");
             capabilities.setCapability("browserstack.console", "warnings");
             capabilities.setCapability("browserstack.networkLogs", "true");
-            capabilities.setCapability("browserstack.networkProfile", "4g-lte-high-latency");
+            //capabilities.setCapability("browserstack.networkProfile", "4g-lte-high-latency");
         }
 
 
